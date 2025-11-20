8025b12dffff05197d61cb69890d7c3cc18028e1
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-20 10:16:15 +0530

    Create without_extra_space.cpp

---
+++ rakesh dsa/array/reverse array/without_extra_space.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void printarr(int * arr,int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<",";
+    }    
+
+}
+
+int main(){
+
+    int arr[]={5,4,3,9,2};
+    int n=sizeof(arr)/sizeof(int);
+
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        // int temp=arr[start];
+        // arr[start]=arr[end];
+        // arr[end]=temp;
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+
+    //print array
+    printarr(arr,n);
+
+    return 0;
+}
