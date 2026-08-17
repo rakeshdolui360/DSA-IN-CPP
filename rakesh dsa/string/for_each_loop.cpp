11b932f07122a637d20f5bd89f09c8311f17b61c
rakeshdolui360 <rakeshdolui52@gmail.com>
2026-08-18 00:46:25 +0530

    Create for_each_loop.cpp

---
+++ rakesh dsa/string/for_each_loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+   
+
+int main(){
+string str="anagram";
+
+
+for(int i=0;i<str.length();i++){
+    cout<<str[i]<<" ";
+}
+
+
+// for each loop to print every character in a string
+for(char ch:str){
+    cout<<ch<<" ";
+}
+
+string str="anagram";
+}
